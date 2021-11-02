5e6b041a912da3d1cd7586d606e25330edbb0c5c
Charles Atchison <52976346+CharlesAtchison@users.noreply.github.com>
2021-11-02 16:10:34 -0500

    Update prepare_c.py
    Changes to the prepare_c.py file and added new way to filter non-words

--- prepare_c.py
+++ prepare_c.py
@@ -3,9 +3,14 @@ import unicodedata
 import re, os
 import json
 
+import seaborn as sns
+import matplotlib.pyplot as plt
+
 import nltk
 from nltk.tokenize.toktok import ToktokTokenizer
 from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import CountVectorizer
+from scipy.stats import zscore
 
 import pandas as pd
 
@@ -117,6 +122,69 @@ def remove_stopwords(string:str, extra_words = [], exclude_words = []) -> str:
 # > * `lemmatized` to hold the lemmatized version of the cleaned data.
 # > * The column data will contain the cleaned data
 
+def remove_words_below_threshold(df: pd.DataFrame, text_col:str, z_cutoff:float, lang=None) -> pd.DataFrame:
+    '''Takes a dataframe and a defined text column and will count the words within that text col
+    then it will remove words from that column that are below a certain z_cutoff.
+    '''
+    if lang:
+        
+        print("Removing words who's zscore falls below the cutoff, this will take a moment")
+        # Create model
+        cv = CountVectorizer()
+
+        # Create bag of words
+        bag_of_words = cv.fit_transform(df[text_col])
+
+        # Create bag of words df
+        bow = pd.DataFrame(bag_of_words.todense(), columns=cv.get_feature_names())
+
+        print('calculating word counts, please wait...')
+        # Calculate word counts
+        word_counts = pd.DataFrame([{'word': word, 'count': int(bow[word].sum())} for word in bow.columns])
+
+        # Average
+        avg = word_counts['count'].mean()
+        # Standard Dev
+        std = word_counts['count'].std()
+
+
+        # calculate word representation percentages
+        word_counts['zscore'] = word_counts['count'].apply(lambda x: (x - avg)/std)
+
+        # print the number of words that are currently in the counts
+        print(f'Before: {len(word_counts)} words in the dataframe')
+
+        # The number of words that that will be removed
+        zdf = word_counts[word_counts.zscore < z_cutoff]
+
+        print(f'After: {len(word_counts) - len(zdf)} words will remain')
+
+        # Set extra words
+        extra_words = list()
+
+        # Add to extra_words the words that are below the z_cutoff
+        extra_words.extend(zdf.word.to_list())
+
+        # Remove the extra words from the defined text_col and then
+        print('Removing the words from the column')
+        df[text_col] = df[text_col].apply(remove_stopwords, extra_words=extra_words)
+
+        # Return the dataframe that has values in the text col
+        df = df[df[text_col] != '']
+    
+    # Check if stemmed is in the dataframe
+    if 'stemmed' not in df.columns:
+        # Stem the cleaned data
+        print('stemming the reduced cleaned data')
+        df['stemmed'] = df['cleaned'].apply(stem)
+    
+    # Check if lemmatized is in the dataframe already
+    if 'lemmatized' not in df.columns:
+        print('lemmatizing the reduced claned data')
+        df['lemmatized'] = df['cleaned'].apply(lemmatize)
+
+    return df
+    
 def prep_readme_data(df:pd.DataFrame, column:str, extra_words=[], exclude_words=[]) -> pd.DataFrame:
     '''
     This function take in a df and the string name for a text column with 
@@ -125,21 +193,15 @@ def prep_readme_data(df:pd.DataFrame, column:str, extra_words=[], exclude_words=
     '''
     # Set the originial to the originial text
     df['original'] = df[column]
-
+        
     # Clean the dataframe within the input column
+    print('cleaning the orginial data')
     df['cleaned'] = df[column].apply(basic_clean).apply(tokenize).apply(remove_stopwords, 
                                    extra_words=extra_words, 
                                    exclude_words=exclude_words)
     
-    df['stemmed'] = df[column].apply(basic_clean).apply(tokenize).apply(stem).apply(remove_stopwords, 
-                                   extra_words=extra_words, 
-                                   exclude_words=exclude_words)
-    
-    df['lemmatized'] = df[column].apply(basic_clean).apply(tokenize).apply(lemmatize).apply(remove_stopwords, 
-                                   extra_words=extra_words, 
-                                   exclude_words=exclude_words)
-    
-    return df
+    # Remove the defined column as it is redundant
+    return df[[col for col in df.columns if col not in [column]]]
 
 def find_dominant_lang(s: str, threshold:float) -> str:
     '''Takes a string of programming languages and associated 
@@ -168,7 +230,7 @@ def find_dominant_lang(s: str, threshold:float) -> str:
     # If no language was greater than the threshold, return None
     return None
 
-def get_readme_data(threshold = 75, extra_words=[], exclude_words=[]) -> pd.DataFrame:
+def get_readme_data(lang=None, lang_threshold = 75, z_cutoff = .5, extra_words=[], exclude_words=[]) -> pd.DataFrame:
     '''Takes threshold for when a programming language is representative of that README
     text. The threshold need to be > 50. Additionally adds the cleaned columns of
     'orginial', 'stemmed', 'lemmatized' from the 'readme' column where the readme
@@ -181,7 +243,11 @@ def get_readme_data(threshold = 75, extra_words=[], exclude_words=[]) -> pd.Data
     '''
     # names the file with the defined threshold at the end with the decimal
     # replaced if it is present
-    filename = f"clean_readme_{str(threshold).replace('.', '_')}.csv"
+    if not lang:
+        lang_s = ''
+    else:
+        lang_s = f'{lang.lower()}_'
+    filename = f"{lang_s}clean_readme_{str(lang_threshold).replace('.', '_')}.csv"
 
     if os.path.exists(filename):
         return pd.read_csv(filename, index_col=[0])
@@ -192,7 +258,7 @@ def get_readme_data(threshold = 75, extra_words=[], exclude_words=[]) -> pd.Data
     # Pull in the source readme data
     df = pd.read_csv('readme_data_c.csv')
     # Filter out all readmes that do not meet the threshold
-    df['prog_lang'] = df.programming_language.apply(lambda x: find_dominant_lang(x, threshold))
+    df['prog_lang'] = df.programming_language.apply(lambda x: find_dominant_lang(x, lang_threshold))
     # Set the dataframe to the languages that returned a value and 
     # reset the index and remove other columns
     df = (df[~df.prog_lang.isna()].reset_index().drop(columns=[
@@ -204,7 +270,47 @@ def get_readme_data(threshold = 75, extra_words=[], exclude_words=[]) -> pd.Data
     # Sends the dataframe and with column readme as the column to be cleaned with the
     # parameters of extra_words and exclude_words as optional parameters.
     df = prep_readme_data(df, 'readme', extra_words=extra_words, exclude_words=exclude_words)
+    
+    def python_and_juypternotebook_comb(s:str) -> str:
+        '''Takes string and checks if it is python or jupyter notebook and returns Python
+        otherwise it returns the same prog lang
+        '''
+        if s.lower() in ['python', 'jupyter notebook']:
+            return 'Python'
+        return s
+    
+    def lang_or_not(s:str, lang:str) -> str:
+        '''Takes string and returns if it is java or not java
+        '''
+        if s.lower() == lang:
+            return lang
+        return f'not_{lang}'
+    if lang:
+        # Sends the prog_lang to combine python and jup notebook
+        df['prog_lang']  = df.prog_lang.apply(python_and_juypternotebook_comb)
 
+        # The non-language string
+        not_lang = f'not_{lang}'
+        # creates the dataframe column name label and labels the dataframe for a label or not
+        df['label']  = df.prog_lang.apply(lambda x: lang_or_not(x, lang))
+
+        # Make a copy of the language_df
+        lang_df = df[df.label == lang].copy()
+        # Make a copy of the non_lang_df
+        not_lang_df = df[df.label != lang].copy()
+        
+        # Send the language_df to have the words below the threshold removed
+        lang_result = remove_words_below_threshold(lang_df, 'cleaned', z_cutoff, lang=lang)
+        # Send the non-language df to haev the words below the threshold removed
+        not_lang_result = remove_words_below_threshold(not_lang_df, 'cleaned', z_cutoff, lang=lang)
+        
+        # Stack the dataframes
+        df = pd.concat([lang_result, not_lang_result], axis=0)
+        
+    else:
+        # Just add the lemmatized and stemmed columns
+        df = remove_words_below_threshold(df, 'cleaned', .5)
+    
     # Save the dataframe to the filename
     df.to_csv(filename)
     # Let's user know that the file was successfully saved and the name
@@ -212,5 +318,3 @@ def get_readme_data(threshold = 75, extra_words=[], exclude_words=[]) -> pd.Data
 
     # Return the dataframe that has been filtered and cleaned
     return df
-
-
